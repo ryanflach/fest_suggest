78720c33931e51f8e6fed5e0338d154f2e69271c
Ryan Flach <ryanflach@users.noreply.github.com>
2016-12-08 10:48:44 -0700

    [WIP] Add playlist functionality to the Spotify service - Current functionality works and is tested, but will be redundant - would be beneficial to utilize low-level caching as seen in the Cacher class so that a request is not made to the Spotify API when the system already has top track data for an artist

--- app/services/spotify/service.rb
+++ app/services/spotify/service.rb
@@ -2,6 +2,7 @@ class Spotify::Service < Base
   def initialize(token)
     @base_url = 'https://api.spotify.com'
     @headers  = { 'Authorization': "Bearer #{token}" }
+    @request_pool = Typhoeus::Hydra.hydra
   end
 
   def recommended_artists(top_5_artist_ids)
@@ -33,10 +34,79 @@ class Spotify::Service < Base
     data[:display_name].nil? ? data[:id] : data[:display_name]
   end
 
+  def create_playlist(name)
+    request = Typhoeus::Request.new(
+      "#{base_url}/v1/users/#{ENV['FS_ADMIN_NAME']}/playlists",
+      method: :post,
+      headers: {
+        'Authorization': headers[:Authorization],
+        'Content-Type': 'application/json'
+      },
+      body: { name: name }.to_json
+    ).run
+    parse(request.response_body)[:id]
+  end
+
+  def each_artists_top_track(artist_ids)
+    requests = artist_ids.map do |id|
+      request = Typhoeus::Request.new(
+        "#{base_url}/v1/artists/#{id}/top-tracks",
+        headers: headers,
+        params: { country: 'US' }
+      )
+      request_pool.queue(request)
+      request
+    end
+
+    request_pool.run
+
+    requests.map do |request|
+      parse(request.response.body)[:tracks].first[:uri]
+    end
+  end
+
+  def add_tracks_to_playlist(playlist_id, tracks)
+    request = Typhoeus::Request.new(
+      "#{base_url}/v1/users/#{ENV['FS_ADMIN_NAME']}" \
+      "/playlists/#{playlist_id}/tracks",
+      method: :post,
+      headers: {
+        'Authorization': headers[:Authorization],
+        'Content-Type': 'application/json'
+      },
+      body: { uris: tracks }.to_json
+    ).run
+    parse(request.response_body)
+  end
+
+  def all_artist_ids(artist_names)
+    requests = artist_names.map do |name|
+      request = Typhoeus::Request.new(
+        "#{base_url}/v1/search?type=artist",
+        headers: headers,
+        params: {
+          q: name.gsub(' ', '+'),
+          type: 'artist',
+          limit: 1
+        }
+      )
+      request_pool.queue(request)
+      request
+    end
+
+    request_pool.run
+
+    requests.reduce([]) do |ids, request|
+      parsed = parse(request.response.body)[:artists][:items]
+      parsed.empty? ? ids : ids << parsed.first[:id]
+    end
+  end
+
   private
 
   attr_reader :base_url,
-              :headers
+              :headers,
+              :request_pool
 
   def request_user_data
     request = Typhoeus::Request.new(
--- spec/services/spotify_service_spec.rb
+++ spec/services/spotify_service_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 include SpotifyHelper
 
 RSpec.describe 'Spotify service' do
-  before(:all) { refresh_access_token if token_expired? }
+  before(:all) { refresh_access_tokens if token_expired? }
 
   context '#get_username' do
     it "gets a user's username" do
@@ -70,6 +70,74 @@ RSpec.describe 'Spotify service' do
         expect(recommended.length).to eq(100)
         expect(recommended.first[:artists].first[:name])
           .to eq('Wye Oak')
+      end
+    end
+  end
+
+  context '#create_playlist' do
+    it 'creates a playlist and returns its Spotify ID' do
+      VCR.use_cassette('spotify_service_create_playlist') do
+        id = Spotify::Service.new(ENV['FS_ACCESS_TOKEN'])
+                             .create_playlist('test')
+
+        expect(id).to be_a(String)
+      end
+    end
+  end
+
+  context '#all_artist_ids' do
+    it 'returns an array of Spotify IDs from a collection of names' do
+      VCR.use_cassette('spotify_service_all_artist_ids') do
+        artists = ['Sufjan Stevens', 'Ryan Flach', 'Team Sleep']
+        ids = Spotify::Service.new(ENV['ACCESS_TOKEN'])
+                              .all_artist_ids(artists)
+
+        expect(ids).to be_a(Array)
+        expect(ids.length).to eq(2)
+        expect(ids.first).to eq('4MXUO7sVCaFgFjoTI5ox5c')
+      end
+    end
+  end
+
+  context '#each_artists_top_track' do
+    it 'returns an array of one Spotify track URI for each ID' do
+      VCR.use_cassette('spotify_service_each_artists_top_track') do
+        ids = ['4MXUO7sVCaFgFjoTI5ox5c', '6CwDvApcRshxhEVMP30Sq7']
+        uris = Spotify::Service.new(ENV['ACCESS_TOKEN'])
+                               .each_artists_top_track(ids)
+
+        expect(uris).to be_a(Array)
+        expect(uris.length).to eq(2)
+        expect(uris.first).to eq('spotify:track:6Rt6KwuF7I8ZkdZG2G0bYr')
+      end
+    end
+  end
+
+  context '#add_tracks_to_playlist' do
+    it 'adds tracks from Spotify URIs to an existing playlist' do
+      VCR.use_cassette('spotify_service_add_tracks_to_playlist') do
+        service = Spotify::Service.new(ENV['FS_ACCESS_TOKEN'])
+        playlist_id = service.create_playlist('test')
+        artist_ids = [
+          '4MXUO7sVCaFgFjoTI5ox5c',
+          '6CwDvApcRshxhEVMP30Sq7'
+        ]
+        track_uris = service.each_artists_top_track(artist_ids)
+        response =
+          service.add_tracks_to_playlist(playlist_id, track_uris)
+        playlist = Typhoeus::Request.new(
+          "#{service.instance_variable_get('@base_url')}/v1/users/" \
+          "#{ENV['FS_ADMIN_NAME']}/playlists/#{playlist_id}/tracks",
+          headers: service.instance_variable_get('@headers')
+        ).run
+        tracks_on_playlist = JSON.parse(
+          playlist.response_body, symbolize_names: true
+        )[:items]
+
+        expect(response).to be_a(Hash)
+        expect(tracks_on_playlist.length).to eq(2)
+        expect(tracks_on_playlist.first[:track][:uri])
+          .to eq(track_uris.first)
       end
     end
   end
