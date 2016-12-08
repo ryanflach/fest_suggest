2298fd17d1e30a2ed1055a5a78ef639c809211e9
Ryan Flach <ryanflach@users.noreply.github.com>
2016-12-07 20:57:18 -0700

    Add ability to find or create a new Playlist with festival data

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,4 +1,11 @@
 class Playlist < ApplicationRecord
   validates :name, presence: true
   validates :spotify_id, presence: true, uniqueness: true
+
+  def self.find_or_create(festival_data)
+    festival_name = festival_data[:displayName]
+    find_or_create_by(name: festival_name) do |playlist|
+      playlist.spotify_id = PlaylistEngine.new(festival_data).id
+    end
+  end
 end
--- spec/models/playlist_spec.rb
+++ spec/models/playlist_spec.rb
@@ -4,4 +4,25 @@ RSpec.describe Playlist, type: :model do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:spotify_id) }
   it { should validate_uniqueness_of(:spotify_id) }
+
+  it 'finds or creates a playlist from festival data' do
+    expect(Playlist.count).to eq(0)
+
+    list = Playlist.create!(name: 'test', spotify_id: 'unique_id')
+
+    expect(Playlist.count).to eq(1)
+    expect(Playlist.find_or_create({ displayName: 'test' })).to eq(list)
+
+    allow_any_instance_of(PlaylistEngine)
+      .to receive(:create_playlist)
+      .and_return('1')
+    other = Playlist.find_or_create({
+      displayName: 'none',
+      performance: []
+    })
+
+    expect(Playlist.count).to eq(2)
+    expect(other.spotify_id).to eq('1')
+    expect(other.name).to eq('none')
+  end
 end
