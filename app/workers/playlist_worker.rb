0dc265a74daf8231df21361da2ca5b3e631ceb2e
Ryan Flach <ryanflach@users.noreply.github.com>
2017-01-01 18:03:44 -0700

    Refactor worker - Moves majority of responsbility to from PlaylistWorker to PlaylistEngine as class methods - Dynamically handle sleep for rate limiting based on the response from Spotify, rather than a static value that was often excessive

--- app/workers/playlist_worker.rb
+++ app/workers/playlist_worker.rb
@@ -2,24 +2,15 @@ class PlaylistWorker
   include Sidekiq::Worker
   sidekiq_options concurrency: 1
 
+  MAX = 25
+
   def perform(playlist_id, artist_names)
     service = Spotify::Service.new(User.site_admin.access_token)
-    artist_ids = []
-    top_tracks = []
 
     until artist_names.empty?
-      ids = service.all_artist_ids(artist_names.shift(25))
-      artist_ids.concat(ids)
-      sleep(15)
+      ids = PlaylistEngine.artist_ids(service, artist_names.shift(MAX))
+      tracks = PlaylistEngine.top_tracks(service, ids)
+      PlaylistEngine.add_tracks(service, playlist_id, tracks)
     end
-
-    until artist_ids.empty?
-      tracks = service.each_artists_top_track(artist_ids.shift(25))
-      top_tracks.concat(tracks)
-      sleep(15)
-    end
-
-    service.add_tracks_to_playlist(playlist_id, top_tracks)
-    puts "Added #{top_tracks.length} tracks to playlist #{playlist_id}."
   end
 end
